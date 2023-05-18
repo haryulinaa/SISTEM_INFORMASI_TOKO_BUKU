a0e9c3de0ec76c33852f7cc30bede5cba68a9680
Tiara Putri Keumala <127756381+Tiaraputrii@users.noreply.github.com>
2023-05-18 15:17:04 +0700

    Add files via upload

---
+++ Penjualan.java
@@ -0,0 +1,7 @@
+public interface Penjualan {
+
+   public void penjualan(String jual);
+    
+}
+
+// Kode di atas adalah sebuah interface Java dengan nama "Penjualan". Interface ini memiliki satu metode yaitu "penjualan" yang mengambil parameter bertipe String. Interface ini bertujuan untuk mendefinisikan kontrak yang harus dipenuhi oleh kelas-kelas lain yang mengimplementasikan interface ini. Dengan kata lain, setiap kelas yang mengimplementasikan interface "Penjualan" harus menyediakan implementasi untuk metode "penjualan" dengan menerima sebuah String sebagai argumen.
---
+++ Religious.java
@@ -0,0 +1,36 @@
+public class Religious extends TokoBuku implements Jenis{
+    public Religious(String namaBuku, String pengarang, String penerbit, int harga) {
+        super(namaBuku, pengarang, penerbit, harga);
+    }
+
+    void showDetail() {
+        System.out.println("Judul Buku  : " + namaBuku);
+        System.out.println("Pengarang   : " + pengarang);
+        System.out.println("Penerbit    : " + penerbit);
+    }
+
+    void showHarga() {
+        System.out.println("Harga Buku  : " + harga);
+    }
+
+    void hitung(int jumlah) {
+        int total = (int) (jumlah * harga);
+        System.out.println("Total       : " + total);
+    }
+
+    public void jenis(String jenis) {
+        System.out.println("Jenis Buku  : " + jenis);
+    }
+
+    public void penjualan(String jual) {
+        if (jual == "Laris") {
+            System.out.println("Termasuk Buku Best Seller");
+        } else if (jual == "Favorit") {
+            System.out.println("Termasuk Buku Favorit");
+        } else if (jual == "Author") {
+            System.out.println("Termasuk Buku dari Author terbaik");
+        } else {
+            System.out.println("Belum di Ketahui Penjualannya");
+        }
+    }
+}
